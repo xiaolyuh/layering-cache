0a76ee194fc2afba2f4b59b537f4ba18140f0738
wyhmonstar <xiaolyuh@163.com>
2018-07-28 21:48:42 +0800

    CacheEvent和CachePut切面

--- layering-cache-aspectj/src/main/java/com/xiaolyuh/aspect/LayeringAspect.java
+++ layering-cache-aspectj/src/main/java/com/xiaolyuh/aspect/LayeringAspect.java
@@ -1,8 +1,6 @@
 package com.xiaolyuh.aspect;
 
-import com.xiaolyuh.annotation.Cacheable;
-import com.xiaolyuh.annotation.FirstCache;
-import com.xiaolyuh.annotation.SecondaryCache;
+import com.xiaolyuh.annotation.*;
 import com.xiaolyuh.cache.Cache;
 import com.xiaolyuh.expression.CacheOperationExpressionEvaluator;
 import com.xiaolyuh.manager.CacheManager;
@@ -31,6 +29,7 @@ import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.Optional;
 
 /**
@@ -88,10 +87,56 @@ public class LayeringAspect {
         }
     }
 
+    @Around("cacheEvictPointcut()")
+    public Object cacheEvictPointcut(ProceedingJoinPoint joinPoint) throws Throwable {
+        CacheOperationInvoker aopAllianceInvoker = () -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable ex) {
+                throw new CacheOperationInvoker.ThrowableWrapperException(ex);
+            }
+        };
+
+        try {
+            // 获取method
+            Method method = this.getSpecificmethod(joinPoint);
+            // 获取注解
+            CacheEvict cacheEvict = AnnotationUtils.findAnnotation(method, CacheEvict.class);
+            // 执行查询缓存方法
+            return executeEvict(aopAllianceInvoker, cacheEvict, method, joinPoint.getArgs(), joinPoint.getTarget());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Around("cachePutPointcut()")
+    public Object cachePutPointcut(ProceedingJoinPoint joinPoint) throws Throwable {
+        CacheOperationInvoker aopAllianceInvoker = () -> {
+            try {
+                return joinPoint.proceed();
+            } catch (Throwable ex) {
+                throw new CacheOperationInvoker.ThrowableWrapperException(ex);
+            }
+        };
+
+        try {
+            // 获取method
+            Method method = this.getSpecificmethod(joinPoint);
+            // 获取注解
+            CachePut cacheEvict = AnnotationUtils.findAnnotation(method, CachePut.class);
+            // 执行查询缓存方法
+            return executePut(aopAllianceInvoker, cacheEvict, method, joinPoint.getArgs(), joinPoint.getTarget());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
     /**
      * 执行Cacheable切面
      *
-     * @param invoker   获取缓存的回调方法
+     * @param invoker   缓存注解的回调方法
      * @param cacheable {@link Cacheable}
      * @param method    {@link Method}
      * @param args      注解方法参数
@@ -125,11 +170,95 @@ public class LayeringAspect {
     }
 
     /**
+     * 执行 CacheEvict 切面
+     *
+     * @param invoker    缓存注解的回调方法
+     * @param cacheEvict {@link CacheEvict}
+     * @param method     {@link Method}
+     * @param args       注解方法参数
+     * @param target     target
+     * @return {@link Object}
+     */
+    private Object executeEvict(CacheOperationInvoker invoker, CacheEvict cacheEvict,
+                                Method method, Object[] args, Object target) {
+
+        // 解析SpEL表达式获取cacheName和key
+        String[] cacheNames = cacheEvict.cacheNames();
+        Assert.notEmpty(cacheEvict.cacheNames(), "缓存名称不能为NULL");
+        // 判断是否删除所有缓存数据
+        if (cacheEvict.allEntries()) {
+            // 删除所有缓存数据（清空）
+            for (String cacheName : cacheNames) {
+                cacheName = generateValue(cacheNames[0], method, args, target).toString();
+                Collection<Cache> caches = cacheManager.getCache(cacheName);
+                for (Cache cache : caches) {
+                    cache.clear();
+                }
+            }
+        } else {
+            // 删除指定key
+            Object key = generateKey(cacheEvict.key(), method, args, target);
+            for (String cacheName : cacheNames) {
+                cacheName = generateValue(cacheNames[0], method, args, target).toString();
+                Collection<Cache> caches = cacheManager.getCache(cacheName);
+                for (Cache cache : caches) {
+                    cache.evict(key);
+                }
+            }
+        }
+
+        // 执行方法
+        return OptionalUnwrapper.unwrap(invoker.invoke());
+    }
+
+    /**
+     * 执行 CachePut 切面
+     *
+     * @param invoker  缓存注解的回调方法
+     * @param cachePut {@link CachePut}
+     * @param method   {@link Method}
+     * @param args     注解方法参数
+     * @param target   target
+     * @return {@link Object}
+     */
+    private Object executePut(CacheOperationInvoker invoker, CachePut cachePut, Method method, Object[] args, Object target) {
+
+
+        String[] cacheNames = cachePut.cacheNames();
+        Assert.notEmpty(cachePut.cacheNames(), "缓存名称不能为NULL");
+        // 解析SpEL表达式获取 key
+        Object key = generateKey(cachePut.key(), method, args, target);
+
+        // 从解决中获取缓存配置
+        FirstCache firstCache = cachePut.firstCache();
+        SecondaryCache secondaryCache = cachePut.secondaryCache();
+        FirstCacheSetting firstCacheSetting = new FirstCacheSetting(firstCache.initialCapacity(), firstCache.maximumSize(),
+                firstCache.expireTime(), firstCache.timeUnit(), firstCache.expireMode());
+
+        SecondaryCacheSetting secondaryCacheSetting = new SecondaryCacheSetting(secondaryCache.expiration(),
+                secondaryCache.preloadTime(), secondaryCache.timeUnit(), secondaryCache.forceRefresh());
+        LayeringCacheSetting layeringCacheSetting = new LayeringCacheSetting(firstCacheSetting, secondaryCacheSetting);
+
+        // 指定调用方法获取缓存值
+        Object result = OptionalUnwrapper.unwrap(invoker.invoke());
+
+        for (String value : cacheNames) {
+            // 解析SpEL表达式获取cacheName
+            String cacheName = generateValue(value, method, args, target).toString();
+            // 通过cacheName和缓存配置获取Cache
+            Cache cache = cacheManager.getCache(cacheName, layeringCacheSetting);
+            cache.putIfAbsent(cache, result);
+        }
+
+        return result;
+    }
+
+    /**
      * 解析SpEL表达式，获取注解上的key属性值
      *
      * @return Object
      */
-    protected Object generateKey(String key, Method method, Object[] args, Object target) {
+    private Object generateKey(String key, Method method, Object[] args, Object target) {
 
         // 获取注解上的key属性值
         Class<?> targetClass = getTargetClass(target);

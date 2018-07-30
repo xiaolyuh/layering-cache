ddb85f5b85a8a093d9aa078c65606ff09096024d
wyhmonstar <xiaolyuh@163.com>
2018-07-30 21:20:33 +0800

    Spring Boot Start

--- layering-cache-starter/src/main/java/com/xiaolyuh/cache/config/LayeringCacheAutoConfig.java
+++ layering-cache-starter/src/main/java/com/xiaolyuh/cache/config/LayeringCacheAutoConfig.java
@@ -2,8 +2,12 @@ package com.xiaolyuh.cache.config;
 
 import com.xiaolyuh.manager.CacheManager;
 import com.xiaolyuh.manager.LayeringCacheManager;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,6 +20,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 @ConditionalOnBean(RedisTemplate.class)
 @ConditionalOnMissingBean(org.springframework.cache.CacheManager.class)
+@AutoConfigureAfter({RedisAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {CacheAutoConfiguration.class})
 public class LayeringCacheAutoConfig {
 
     @Bean

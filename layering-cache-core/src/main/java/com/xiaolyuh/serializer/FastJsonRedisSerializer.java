582b152a4819b717096c07b68501898ba28b456c
wyhmonstar <xiaolyuh@163.com>
2018-08-20 13:06:03 +0800

    fix bug

--- layering-cache-core/src/main/java/com/xiaolyuh/serializer/FastJsonRedisSerializer.java
+++ layering-cache-core/src/main/java/com/xiaolyuh/serializer/FastJsonRedisSerializer.java
@@ -2,7 +2,7 @@ package com.xiaolyuh.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import org.springframework.cache.support.NullValue;
+import com.xiaolyuh.support.NullValue;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
